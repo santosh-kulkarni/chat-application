65a64d0708fbc5b887081558abcd6ad469e07dfe
SRINIVAS-B-SINDAGI <srinivas.sindagi@hebbalelabs.com>
2019-02-22 20:19:20 +0530

    Testing signUp function

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-
-$(document).ready(function() {
-    $("#msg").on("keyup", function(event) {
-        if(event.keyCode === 13) {
+function signUp() {
+    console.log('hello world');
+}
+$(document).ready(function () {
+    $("#msg").on("keyup", function (event) {
+        if (event.keyCode === 13) {
             storeData();
         }
     });
@@ -21,33 +23,33 @@ function listenForChildChangeEvent() {
         window.location.href = "login.html";
     }
     var ref = firebase.database().ref("message");
-    ref.on("child_added", function(snapshot) {
+    ref.on("child_added", function (snapshot) {
         var html = "";
         snapshot = snapshot.val();
-        if(snapshot["name"] === "Santosh") {
+        if (snapshot["name"] === "Santosh") {
             html = html + "<span style='padding: 5px 10px 5px 10px; background-color: #dcf8c6; margin-bottom: 10px; float: right;'>";
-            html  = html + snapshot["name"] + " </br> " + snapshot["message"] ;
+            html = html + snapshot["name"] + " </br> " + snapshot["message"];
             html = html + "</span> </br> </br> </br>";
         }
         else {
             html = html + "<span style='padding: 5px 10px 5px 10px; background-color: #ffffff; margin-bottom: 10px; float: left;'>";
-            html  = html + snapshot["name"] + " </br> " + snapshot["message"] ;
+            html = html + snapshot["name"] + " </br> " + snapshot["message"];
             html = html + "</span> </br> </br> </br>";
-        }    
-        $(document).ready(function() {
+        }
+        $(document).ready(function () {
             $("#data").append(html);
             $("#scrollDown").scrollTop($("#scrollDown")[0].scrollHeight);
         });
     });
 
     ref = firebase.database().ref("users");
-    ref.once("value", function(snapshot) {
+    ref.once("value", function (snapshot) {
         snapshot = snapshot.val();
         var html = "";
-        for(val in snapshot) {
+        for (val in snapshot) {
             html = html + "<div> <img  src='man.png' style='display: inline; width: 30px; height: 30px;' alt='Profile Photo' /> <h5 style='padding-left: 10px; display: inline'>" + snapshot[val]["displayName"] + "</h5></div><hr>";
         }
-        $(document).ready(function() {
+        $(document).ready(function () {
             $("#usersData").html(html);
         });
     });
@@ -57,25 +59,25 @@ function storeData() {
     var newMSG = database.push();
     console.log(document.getElementById("msg").value);
     newMSG.set({
-        name : "Santosh",
-        message : document.getElementById("msg").value
+        name: "Santosh",
+        message: document.getElementById("msg").value
     });
     document.getElementById("msg").value = "";
 }
 function login() {
     var email = document.getElementById('emailid').value;
     var pass = document.getElementById("pass").value;
-    firebase.auth().signInWithEmailAndPassword(email, pass).catch(function(error) {
+    firebase.auth().signInWithEmailAndPassword(email, pass).catch(function (error) {
         var errorMessage = error.message;
         alert(errorMessage);
     });
-    firebase.auth().onAuthStateChanged(function(user) {
+    firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
-           sessionStorage.email = user.emailVerified;
-           window.location.href = "home.html";
-        } 
+            sessionStorage.email = user.emailVerified;
+            window.location.href = "home.html";
+        }
         else {
             sessionStorage.email = null;
         }
-      });
+    });
 }
